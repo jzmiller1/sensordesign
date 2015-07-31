290f906738e861e8288009b2648a520c3fea2490
jpdees0754 <jpdees0754@ung.edu>
2015-07-31 13:02:01 -0400

    Implemented sqlite3 database and eliminated SciPy dependence.
    Replaced Scipy integration procedures with algebraic interpolation
    from sample data stored in database.
    Added sqlite3 database functionality whereby sample material reflectance data sets from the USGS can be downloaded and stored.
    Added material reflectance curve viewing option to main menu.
    Updated internal and external documentation.

--- scenarios.py
+++ scenarios.py
@@ -1,10 +1,15 @@
 import copy
-
 from materials import three_materials, two_materials
 import sensing as rs
 
 
 class Scenario():
+    """This class models a given remote sensing scenario as defined by the
+    number of materials involved and mixing within pixels. Mixing can be
+     constrained (static percentage values for mixing) or unconstrained
+     (random percentage values for mixing).
+
+    """
     def __init__(self, name, materials,
                  terrain=None,
                  atmosphere=None,
@@ -20,23 +25,28 @@ class Scenario():
             self.spectral_mixing = {'mixing': False, 'constrained': True}
 
     def generate_terrain(self, mixing=False, constrained=True):
+        #Generates a 3x3 grid of remotely sensed pixel values based
+        #on Scenario parameters and stores them in self.terrain.
         self.terrain = rs.create_grid(self.materials,
                                       mixed=mixing,
                                       constrained=constrained)
 
     def display(self):
+        #displays pixel values from self.terrain for user interpretation
+        #then displays materials and percentages which yielded those values.
         print("Remotely Sensed Image for Interpretation\n")
         for x in range(3, 10, 3):
             print(self.image[x-3:x])
-
+        print('\n')
         raw_input("""Press any key to reveal terrain.\n""")
         for x in range(3, 10, 3):
             print([m.name for m in self.terrain][x-3:x])
+        print('\n')
         raw_input("""Press any key to end scenario.\n""")
 
     def execute(self):
         ## GENERATE REFLECTANCE CURVE PLOT
-        print("Start by reviewing the reflectance curves.")
+        print("Start by reviewing the reflectance curves.\n")
         rs.plot(self.base_materials)
 
         ## CREATE BANDS
@@ -65,6 +75,7 @@ scenarii = [Scenario('Two Materials', two_materials),
             Scenario('Unconstrained Linear Spectral Mixing',
                      two_materials,
                      spectral_mixing={'mixing': True,
-                                      'constrained': False})]
+                                      'constrained': False})
+            ]
 
 
